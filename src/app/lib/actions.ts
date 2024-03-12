98b515a213f946268e4de3b46bdc274b73229f0e
Ethan Skalski <ethanskalski@Ethans-MacBook-Pro.local>
2024-03-12 17:12:42 -0400

    image upload and styling

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -1,23 +1,12 @@
 'use server';
-import { revalidatePath } from 'next/cache';
-import fs from 'fs';
 
-export async function uploadImage(formData: Object) {
-  console.log('Uploading image');
-  console.log('Form data:', formData);
-  const image = formData;
-  console.log('Image:', image);
-  if (image === null) {
-    console.error('No image found');
-  } else {
-    const imageBuffer = await (image as Blob).arrayBuffer();
-    fs.writeFile('@/uploads', Buffer.from(imageBuffer), (err) => {
-      if (err) {
-        console.error('Error saving image:', err);
-      } else {
-        console.log('Image saved successfully');
-      }
-    });
-  }
-  revalidatePath('/');
+import { revalidatePath } from 'next/cache';
+import { redirect } from "next/navigation";
+
+let imgURL: string;
+
+export async function uploadImage(formData: FormData) {
+  const image = formData.get('image');
+  if (image) imgURL = URL.createObjectURL(image as File);
+  console.log('imgURL: ',imgURL)
 }
