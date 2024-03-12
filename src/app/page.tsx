304feeb9df51bbb1ac98ccf81817cc39d0ee2996
Ethan Skalski <ethanskalski@Ethans-MacBook-Pro.local>
2024-03-12 17:12:42 -0400

    image upload and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,32 +2,54 @@
 
 import Image from 'next/image';
 import styles from './page.module.css';
-import { uploadImage } from './lib/actions';
+// import { uploadImage } from './lib/actions';
+import { useState } from 'react';
 
 export default function Home() {
+  const [imgURL, setImgURL] = useState('');
+
+  let inputImage: File;
+
+  const handleClick = () => {
+    if (inputImage) setImgURL(URL.createObjectURL(inputImage as File));
+  };
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          <strong>xFlair</strong>
-        </p>
-        <div>
-            <Image
-              src="/logo.png"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={150}
-              priority
-            />
+      <Image
+        src='/logoBlack.png'
+        alt='Logo'
+        className={styles.Logo}
+        width={396}
+        height={512}
+        priority
+      />
+      <div className={styles.majorDiv}>
+        <div className={styles.title}>
+          <h1>xFlair</h1>
         </div>
-      </div>
-
-      <div>
-        <form action={uploadImage}>
-          <input type="file" accept="image/*" />
-          <button type="submit">Upload</button>
-        </form>
+        <div className={styles.inputBox}>
+          <img src={imgURL} alt='Upload an image to begin' />
+          <div>
+            {imgURL && <button className = {styles.dataVis}>Analysis Visualization</button>}
+          </div>
+          <div>
+            {/* <form action={uploadImage}>
+            <input id='image' name='image'type='file' accept='image/*' />
+            <button type='submit'>Upload</button>
+          </form> */}
+            <input
+              className={styles.fileBrowser}
+              name='image'
+              type='file'
+              accept='image/*'
+              onChange={(e) => (inputImage = e.target.files[0])}
+            />
+            <label>
+              <button className={styles.fileBrowser} id = 'upload' onClick={handleClick}>Upload</button>
+            </label>
+          </div>
+        </div>
       </div>
     </main>
   );
