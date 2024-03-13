fa606f6471965a6ffeac0370d0eb6d59b7c84724
Ethan Skalski <ethanskalski@Ethans-MacBook-Pro.local>
2024-03-13 15:04:41 -0400

    gen heatmap data

--- src/app/ui/heatmap.tsx
+++ src/app/ui/heatmap.tsx
@@ -6,8 +6,8 @@ export default function Heatmap() {
 
   useEffect(() => {
     const margin = { top: 30, right: 30, bottom: 30, left: 30 };
-    const width = 450 - margin.left - margin.right;
-    const height = 450 - margin.top - margin.bottom;
+    const width = 900 - margin.left - margin.right;
+    const height = 900 - margin.top - margin.bottom;
 
     const svg = d3
       .select(svgRef.current)
@@ -16,28 +16,18 @@ export default function Heatmap() {
       .append('g')
       .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-    const myGroups = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
-    const myVars = [
-      'v1',
-      'v2',
-      'v3',
-      'v4',
-      'v5',
-      'v6',
-      'v7',
-      'v8',
-      'v9',
-      'v10',
-    ];
+    const rowData = Array.from(Array(224).keys()).map((el) => `r${el}`);
 
-    const x = d3.scaleBand().range([0, width]).domain(myGroups).padding(0.01);
+    const colData = Array.from(Array(224).keys()).map((el) => `c${el}`);
+
+    const x = d3.scaleBand().range([0, width]).domain(rowData).padding(0.01);
 
     svg
       .append('g')
       .attr('transform', `translate(0, ${height})`)
       .call(d3.axisBottom(x));
 
-    const y = d3.scaleBand().range([height, 0]).domain(myVars).padding(0.01);
+    const y = d3.scaleBand().range([height, 0]).domain(colData).padding(0.01);
 
     svg.append('g').call(d3.axisLeft(y));
 
@@ -46,7 +36,7 @@ export default function Heatmap() {
       .range(['white', '#69b3a2'])
       .domain([1, 100]);
 
-    d3.csv('/heatmapdata.csv').then((data) => {
+    d3.csv('/heatmap_data.csv').then((data) => {
       svg
         .selectAll()
         .data(data, (d: any) => d.group + ':' + d.variable)
