6c717bd2671e4328bc27a3307d1624feb47c12f9
Charlie Schubach <charlie.schubach@gmail.com>
2024-03-21 19:09:12 -0400

    mvp

--- src/app/ui/gridMapGif.tsx
+++ src/app/ui/gridMapGif.tsx
@@ -1,17 +1,19 @@
-import {useEffect, useRef, useState} from 'react';
-import Image from "next/image";
+import { Suspense, useEffect, useRef, useState } from 'react';
+import styles from '../page.module.css';
+import Image from 'next/image';
 
-let gridMapGif: File | undefined; 
+let gridMapGif: File | undefined;
 
-export default function GridMapGif () {
-    const [gridURL, setGridURL] = useState("");
-    const gridMap = () => {
-        setGridURL(`File Name: ${gridMapGif?.name}`)
-    }
-    
-    return <img
-    className = "gridMapGif"
-     src = {gifURL}
-     alt = "gridMapGif"
-     />;
+export default function GridMapGif(props) {
+  useEffect(() => {
+    console.log(props.fgifURL);
+  }, [props.fgifURL]);
+
+  return (
+    <div className={styles.embla__slide}>
+      <Suspense fallback={<img src="/loadspinner.gif" alt="loading" />}>
+        <img className={styles.otherImg} src={props.fGifURL} alt="gridMapGif" />
+      </Suspense>
+    </div>
+  );
 }
