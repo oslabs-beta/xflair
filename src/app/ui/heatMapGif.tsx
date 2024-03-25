62e25da60d5cefb51d6b64e4ebd4fa026468e69a
Ethan Skalski <ethanskalski@Ethans-MacBook-Pro.local>
2024-03-25 14:28:26 -0400

    working router

--- src/app/ui/heatMapGif.tsx
+++ src/app/ui/heatMapGif.tsx
@@ -1,22 +1,45 @@
+// 'use server';
+
 import { useEffect, useRef, useState, Suspense } from 'react';
 import Image from 'next/image';
 import { style } from 'd3';
 import styles from '../page.module.css';
 
-let gifImage: File | undefined;
+// const fetchHmap = async (data: string, filePath: string) => {
+//   const res = await fetch('http://localhost:3001/api/heatmap', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ data, filePath }),
+//     })
+//   const resParsed = await res.json();
+//   return resParsed.filePath;
+// }
+
+// const fetchHgif = async (hFilePath: string) => {
+//   const res = await fetch('http://localhost:3001/api/hgif', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ hFilePath }),
+//     })
+//   const resParsed = await res.json();
+//   return resParsed.hGifUrl;
+// }
 
 export default function HeatMapGif(props) {
-  // const [gifURL, setGifURL] = useState("");
-  // const heatMap = () => {
-  //     setGifURL(`File Name: ${gifImage?.name}`)
-  // }
-  useEffect(() => { console.log(props.hgifURL) }, [props.hgifURL])
+  const data = 'test1';
+  const filePath = 'test2';
+
+  // const hFilePath = await fetchHmap(data, filePath);
+  // const hGifUrl = await fetchHgif(hFilePath);
 
   return (
     <div className={styles.embla__slide}>
-        <Suspense fallback={<img src='/loadspinner.gif' alt='loading' />}>
-        <img className={styles.otherImg} src={props.hGifURL} alt="heatMapGif" />
-      </Suspense> 
+      <img className={styles.otherImg} src={props.hGifURL} alt='heatMapGif' />
+      {/* <img className={styles.otherImg} src={hGifUrl} alt="heatMapGif" /> */}
     </div>
   );
 }
