a0bbbe422909bcd92db8b1ce23f05a8ab2012d53
Jimmy Ho <jimmyho@Jimmys-Macbook-Air.local>
2024-03-25 10:32:17 -0400

    css browse and upload combined, centered around solar flare

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,59 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import styles from './page.module.css';
-import { generateHeatmaps, createGif } from './lib/actions';
-import { lazy, Suspense, useState, useEffect, ChangeEvent } from 'react';
-import { modelOutput } from './lib/definitions';
-import HeatMapGif from './ui/heatMapGif';
-import GridMapGif from './ui/gridMapGif';
-import ResizedImage from './ui/resizedImage';
-import Top5 from './ui/top5Classes';
-import axios from 'axios';
+import Image from "next/image";
+import styles from "./page.module.css";
+import { generateHeatmaps, createGif } from "./lib/actions";
+import { lazy, Suspense, useState, useEffect, ChangeEvent } from "react";
+import { modelOutput } from "./lib/definitions";
+import HeatMapGif from "./ui/heatMapGif";
+import GridMapGif from "./ui/gridMapGif";
+import ResizedImage from "./ui/resizedImage";
+import Top5 from "./ui/top5Classes";
+import axios from "axios";
 // import Heatmap from './ui/heatmap';
-import Modal from './ui/modal';
+import Modal from "./ui/modal";
 
-const Heatmap = lazy(() => import('./ui/heatmap'));
+const Heatmap = lazy(() => import("./ui/heatmap"));
 
 let inputImage: File | undefined;
 
 export default function Home() {
   const states = [
-    'original',
-    'preprocessing',
-    'preprocessed',
-    'loadingMesh',
-    'featureMap',
-    'resizing',
-    'classifying',
-    'output',
+    "original",
+    "preprocessing",
+    "preprocessed",
+    "loadingMesh",
+    "featureMap",
+    "resizing",
+    "classifying",
+    "output",
   ];
 
   const [currentState, setCurrentState] = useState(0);
-  const [preProcessedImage, setPreprocessedImage] = useState('');
-  const [featureMap, setFeatureMap] = useState('');
-  const [classificationResult, setClassificationResult] = useState('');
-  const [imgName, setImgName] = useState('Browse...');
-  const [imgURL, setImgURL] = useState('');
+  const [preProcessedImage, setPreprocessedImage] = useState("");
+  const [featureMap, setFeatureMap] = useState("");
+  const [classificationResult, setClassificationResult] = useState("");
+  const [imgName, setImgName] = useState("Browse...");
+  const [imgURL, setImgURL] = useState("");
   const [vizState, setVizState] = useState(false);
-  const [predictionName, setPredictionName] = useState('');
+  const [predictionName, setPredictionName] = useState("");
   const [viz, openViz] = useState(false);
-  const [hGifURL, setHGifURL] = useState('');
-  const [fGifURL, setFGifURL] = useState('');
+  const [hGifURL, setHGifURL] = useState("");
+  const [fGifURL, setFGifURL] = useState("");
   const [initialTime, setInitialTime] = useState(0);
   const [finalTime, setFinalTime] = useState(0);
   const [time, setTime] = useState(0);
   const [top5, setTop5] = useState({});
-  const [preprocessFilePath, setPreprocessFilePath] = useState('');
-  const [imagePath, setImagePath] = useState('');
-  const [hFilePath, setHFilePath] = useState('');
-  const [fFilePath, setFFilePath] = useState('');
+  const [preprocessFilePath, setPreprocessFilePath] = useState("");
+  const [imagePath, setImagePath] = useState("");
+  const [hFilePath, setHFilePath] = useState("");
+  const [fFilePath, setFFilePath] = useState("");
+  const [buttonState, setButtonState] = useState(false);
 
   const browse = (e: ChangeEvent<HTMLInputElement>) => {
     inputImage = e.currentTarget.files?.[0];
     setImgName(`File Name:  ${inputImage?.name}`);
+    setButtonState(true);
     if (inputImage) {
       setImgURL(URL.createObjectURL(inputImage));
     }
@@ -60,46 +62,46 @@ export default function Home() {
   useEffect(() => {
     if (!initialTime || !finalTime) return;
     setTime(finalTime - initialTime);
-    console.log('time:', time);
+    console.log("time:", time);
   }, [finalTime, initialTime, time]);
 
   const predictions = (data, filePath) => {
-    fetch('http://localhost:3001/api/predictions', {
-      method: 'POST',
+    fetch("http://localhost:3001/api/predictions", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ data, filePath }),
     })
       .then((response) => response.json())
       .then((response) => {
-        console.log('response:', response);
+        console.log("response:", response);
         setPredictionName(response.class);
-        console.log('top5:', JSON.parse(response.top5.replace(/'/g, '"')));
+        console.log("top5:", JSON.parse(response.top5.replace(/'/g, '"')));
         setTop5(JSON.parse(response.top5.replace(/'/g, '"')));
         setFinalTime(Date.now());
       });
 
-    fetch('http://localhost:3001/api/preprocess', {
-      method: 'POST',
+    fetch("http://localhost:3001/api/preprocess", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ data, filePath }),
     })
       .then((response) => response.json())
       .then((response) => {
-        console.log('response:', response);
+        console.log("response:", response);
         setPreprocessFilePath(response.filePath);
         setVizState(true);
       });
   };
 
   const maps = (data, filePath) => {
-    fetch('http://localhost:3001/api/heatmap', {
-      method: 'POST',
+    fetch("http://localhost:3001/api/heatmap", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ data, filePath }),
     })
@@ -108,10 +110,10 @@ export default function Home() {
         setHFilePath(response.filePath);
       });
 
-    fetch('http://localhost:3001/api/feature', {
-      method: 'POST',
+    fetch("http://localhost:3001/api/feature", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ data, filePath }),
     })
@@ -126,16 +128,16 @@ export default function Home() {
       return;
     }
 
-    fetch('http://localhost:3001/api/hgif', {
-      method: 'POST',
+    fetch("http://localhost:3001/api/hgif", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ hFilePath }),
     })
       .then((response) => response.json())
       .then((response) => {
-        console.log('response:', response.hgifUrl)
+        console.log("response:", response.hgifUrl);
         setHGifURL(response.hgifUrl);
       });
   }, [hFilePath]);
@@ -145,10 +147,10 @@ export default function Home() {
       return;
     }
 
-    fetch('http://localhost:3001/api/fgif', {
-      method: 'POST',
+    fetch("http://localhost:3001/api/fgif", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ fFilePath }),
     })
@@ -164,18 +166,18 @@ export default function Home() {
       setInitialTime(Date.now());
 
       const formData = new FormData();
-      formData.append('file', inputImage as File);
+      formData.append("file", inputImage as File);
 
       await axios
-        .post('http://localhost:3001/api/imageSave', formData, {
+        .post("http://localhost:3001/api/imageSave", formData, {
           headers: {
-            'Content-Type': 'multipart/form-data',
+            "Content-Type": "multipart/form-data",
           },
         })
         .then((response) => {
-          console.log('response:', response.data);
+          console.log("response:", response.data);
           setImagePath(response.data);
-          console.log('imagePath:', imagePath);
+          console.log("imagePath:", imagePath);
         })
         .catch((error) => {
           console.error(error);
@@ -191,10 +193,10 @@ export default function Home() {
     reader.readAsDataURL(inputImage as File);
 
     reader.onloadend = () => {
-      const data = (reader.result as string).split(',')[1];
+      const data = (reader.result as string).split(",")[1];
       // tensorflowJS(imageData);
       // analyze(data, imagePath);
-      console.log('imagePath:', imagePath);
+      console.log("imagePath:", imagePath);
       predictions(data, imagePath);
       maps(data, imagePath);
     };
@@ -202,13 +204,13 @@ export default function Home() {
 
   const clearClick = () => {
     inputImage = undefined;
-    setImgName('Browse...');
-    setImgURL('');
-    setFFilePath('');
-    setHFilePath('');
-    setImagePath('');
-    setHGifURL('');
-    setFGifURL('');
+    setImgName("Browse...");
+    setImgURL("");
+    setFFilePath("");
+    setHFilePath("");
+    setImagePath("");
+    setHGifURL("");
+    setFGifURL("");
     setVizState(false);
   };
   //ori
@@ -228,24 +230,24 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-
       <div className={styles.majorDiv}>
         <div className={styles.title}>
-          <div className = {styles.column}>
-      <Image
-        src="/logoBlack.png"
-        alt="Logo"
-        className={styles.Logo}
-        width={277/2}
-        height={358/2}
-        priority
-      />
-      </div>
-      <div className = {styles.column}>
-          <img className={styles.titleImg} src="/title.png" alt="titleText" />
+          <div className={styles.column}>
+            {/* <Image
+              src="/logoi
+              .png"
+              alt="Logo"
+              className={styles.Logo}
+              width={277 / 2}
+              height={358 / 2}
+              priority
+            /> */}
+          </div>
+          <div className={styles.column}>
+            <img className={styles.titleImg} src="/title.png" alt="titleText" />
           </div>
         </div>
-
+        {/* <div className={styles.inputBoxBackground}> */}
         <div className={styles.inputBox}>
           {viz && (
             <Modal
@@ -266,13 +268,13 @@ export default function Home() {
           )}
           {vizState && (
             <>
-              <h2 className="predictedClassName">Class:   {predictionName}</h2>
+              <h2 className="predictedClassName">Class: {predictionName}</h2>
               {time > 0 && <h3>Time: {(time / 1000).toFixed(2)} seconds</h3>}
             </>
           )}
-          {!imgURL && (
+          {/* {!imgURL && (
             <p className={styles.placeholderText}>Waiting for image</p>
-          )}
+          )} */}
 
           <div>
             {vizState && (
@@ -280,31 +282,35 @@ export default function Home() {
                 Analysis Visualization
               </button>
             )}
+            <div className={styles.buttonBox}>
+              {!buttonState && (
+                <label className={styles.imgInput}>
+                  <text className = {styles.browseText}>{imgName}</text>
+                  <input
+                    className={styles.hide}
+                    name="image"
+                    type="file"
+                    accept="image/*"
+                    onChange={(e) => browse(e)}
+                  />
+                </label>
+              )}
+              {buttonState && (
+                <button
+                  className={styles.primaryBtn}
+                  id="upload"
+                  onClick={uploadClick}
+                >
+                  Upload
+                </button>
+              )}
+            </div>
           </div>
-
-          <div className={styles.buttonBox}>
-            <label className={styles.imgInput}>
-              <text>{imgName}</text>
-              <input
-                className={styles.hide}
-                name="image"
-                type="file"
-                accept="image/*"
-                onChange={(e) => browse(e)}
-              />
-            </label>
-            <button
-              className={styles.primaryBtn}
-              id="upload"
-              onClick={uploadClick}
-            >
-              Upload
-            </button>
+          {/* </div> */}
+        </div>
             <button className={styles.altBtn} onClick={clearClick}>
               Clear
             </button>
-          </div>
-        </div>
       </div>
     </main>
   );
