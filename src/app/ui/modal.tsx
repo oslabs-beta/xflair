b60dff23a781c1cf9da2fb03fef01dbd547fb8f3
Ethan Skalski <ethanskalski@Ethans-MacBook-Pro.local>
2024-03-28 10:49:58 -0400

    page organized async render not working

--- src/app/ui/modal.tsx
+++ src/app/ui/modal.tsx
@@ -2,36 +2,41 @@ import styles from '../page.module.css';
 import { EmblaCarousel } from './carousel';
 import { Suspense, lazy } from 'react';
 
+// import HeatMapGif from './heatMapGif';
+const HeatMapGif = lazy(() => import('./heatMapGif'));
 const Heatmap = lazy(() => import('./heatmap'));
 
 export default function Modal(props) {
   return (
     <div className={styles.modalcontainer}>
       <div className={styles.modaltitlecontainer}>
-        <div className={styles.test}/>
+        <div className={styles.test} />
         <h1 className={styles.modaltitle}>Analysis</h1>
-        <svg
+        {/* <svg
           onClick={props.closeViz}
           className={styles.modalxicon}
           xmlns='http://www.w3.org/2000/svg'
           width='200'
           height='200'
           viewBox='0 0 24 24'
-        ></svg>
+        /> */}
+        <h1 onClick={props.closeViz} style={{ color: 'white' }}>
+          X
+        </h1>
       </div>
-      {/* <Suspense fallback={<img src='/loadspinner.gif' alt='loading' />}>
-        <Heatmap />
-      </Suspense> */}
-      <EmblaCarousel closeViz={props.closeViz} 
+      <Suspense fallback={<img src='/loadspinner.gif' alt='loading' />}>
+        <HeatMapGif />
+      </Suspense>
+      {/* <EmblaCarousel closeViz={props.closeViz} 
           hGifURL={props.hGifURL}
           fGifURL={props.fGifURL}
           top5={props.top5}
-          preprocessFilePath={props.preprocessFilePath}/>
-      {/* <div className={styles.modalbutton}>
+          preprocessFilePath={props.preprocessFilePath}/> */}
+      <div className={styles.modalbutton}>
         <button className={styles.primaryBtn} onClick={props.closeViz}>
           Okay
         </button>
-      </div> */}
+      </div>
     </div>
   );
 }
