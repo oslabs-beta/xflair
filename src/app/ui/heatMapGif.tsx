860ee8641a1b703b076be7bfd0a4c5291463b53e
Jimmy Ho <jimmyho@Jimmys-Macbook-Air.local>
2024-03-28 15:14:04 -0400

    frontend overhaul: tailwind merged, routing implemented, general bug fixes
    Co-authored-by: Ethan <ethan@ethanskalski.com>

--- src/app/ui/heatMapGif.tsx
+++ src/app/ui/heatMapGif.tsx
@@ -1,12 +1,8 @@
-'use server';
-
-import { useEffect, useRef, useState, Suspense } from 'react';
-import Image from 'next/image';
-import { style } from 'd3';
+import {Suspense} from 'react';
 import styles from '../page.module.css';
-import { fetchHgif } from '../lib/actions';
+// import { fetchHgif } from '../lib/actions';
 
-export default async function HeatMapGif(props) {
+export default function HeatMapGif(props) {
   const data = 'test1';
   const filePath = 'test2';
 
@@ -14,10 +10,12 @@ export default async function HeatMapGif(props) {
   const hGifUrl = 'https://res.cloudinary.com/dufc4fu9r/image/multi/f_gif/v1711062935/heatmap_gif.gif'
 
   return (
-    // <div className={styles.embla__slide}>
-    <div>
+    <div className={styles.embla__slide}>
+    {/* <div> */}
       {/* <img className={styles.otherImg} src={props.hGifURL} alt='heatMapGif' /> */}
+      <Suspense fallback={<img src="/loadspinner.gif" alt="loading" />}>
       <img className={styles.otherImg} src={hGifUrl} alt='heatMapGif' />
+      </Suspense>
     </div>
   );
 }
