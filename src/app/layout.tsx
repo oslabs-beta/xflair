a1c2d19a73cb871568c6eca146fd05af3ba65177
devincarrick <devin.carrick@gmail.com>
2024-04-03 10:23:02 -0400

    Add Google Fonts link to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({ children }) {
       <Head>
         <title>xFlair Demo</title>
         <meta name='description' content='A demo for xFlair' />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <main className={inter.className}>
